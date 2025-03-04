c16ded3a860ded54d308d8965e8f1b63fce71833
Alexandra Squirrel <aleksandratarasova4@gmail.com>
2025-03-04 20:55:38 +0300

    Home page addition. New locators. Element text function

--- pages/base_page.py
+++ pages/base_page.py
@@ -1,9 +1,53 @@
+import math
 from selenium import webdriver as remote_web_driver
+from selenium.common.exceptions import NoSuchElementException
+from selenium.common.exceptions import NoAlertPresentException
+
 
 class BasePage:
-    def __init__(self, browser: remote_web_driver, url):
+    def __init__(self, browser : remote_web_driver, url, timeout=10):
         self.browser = browser
         self.url = url
+        self.browser.implicitly_wait(timeout)
 
     def open(self):
         self.browser.get(self.url)
+
+    def is_element_present(self, how, what):
+        try:
+            self.browser.find_element(how, what)
+        except NoSuchElementException:
+            return False
+        return True
+
+    def find_element_text(self, how, what):
+        try:
+            element = self.browser.find_element(how, what)
+            return element.text
+        except NoSuchElementException:
+            return False
+
+    # Проверка, что подстрока "login" есть в текущем url браузера.
+    def is_url_correct(self, right_substr):
+        current_url = self.browser.current_url
+        result = current_url.find(right_substr)
+        if result == -1:
+            return False
+        else:
+            return True
+
+    # Посчитать результат математического выражения и ввести ответ.
+    # Этот метод нужен только для проверки того, что вы написали тест на Selenium.
+    def solve_quiz_and_get_code(self):
+        alert = self.browser.switch_to.alert
+        x = alert.text.split(" ")[2]
+        answer = str(math.log(abs((12 * math.sin(float(x))))))
+        alert.send_keys(answer)
+        alert.accept()
+        try:
+            alert = self.browser.switch_to.alert
+            alert_text = alert.text
+            print(f"Your code: {alert_text}")
+            alert.accept()
+        except NoAlertPresentException:
+            print("No second alert presented")
--- pages/locators.py
+++ pages/locators.py
@@ -1,13 +1,21 @@
 from selenium.webdriver.common.by import By
 
-class MainPageLocators():
+class MainPageLocators:
     LOGIN_LINK = (By.CSS_SELECTOR, "#login_link")
     LOGIN_LINK_2 = (By.CSS_SELECTOR, "#registration_link")
 
-class LoginPageLocators():
+class LoginPageLocators:
     # should_be_login_url
     # should_be_login_form
     # should_be_register_form
     # LOGIN_URL = (By.CSS_SELECTOR, "#login_link")
     LOGIN_FORM = (By.CSS_SELECTOR, "#login_form")
     REGISTER_FORM = (By.CSS_SELECTOR, "#register_form")
+
+class ProductPageLocators:
+    ADD_TO_CART_BUTTON = (By.CLASS_NAME, "btn-add-to-basket")
+    TITLE_OF_PRODUCT = (By.CSS_SELECTOR, "div.product_main > h1")
+    ADDED_TO_CART_TITLE = (By.CSS_SELECTOR, "#messages > div:nth-child(1) > div > strong")
+    PRODUCT_PRICE = (By.CSS_SELECTOR, "div.product_main > .price_color") # 23,99 £
+    CART_COST_MESSAGE = (By.CSS_SELECTOR, "#messages > div > div > p:nth-child(1) > strong") # 23,99 £
+
